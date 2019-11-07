db45b86b09b934bd23f0b68229562c70d6e89437
Leah King <S00187348@mail.itsligo.ie>
2019-11-07 11:35:02 +0000

    Add project files.

---
+++ CA1/Subject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    public abstract class Subject
+    {
+        public string Name { get; set; }
+        public string Lecturer { get; set; }
+        public int Year { get; set; }
+    }
+    #region Constructors
+    /*public Student(string name, string lecturer, int year)
+    {
+         Name = name;
+        Lecturer = lecturer;
+        Year =year;
+
+    }
+    public Student (string name, string lecturer,int year)
+           : this(name, lecturer, year.now)
+    {
+
+    }*/
+    #endregion Constructors
+
+    public override string ToString()
+    {
+        return string.Format($"{Name}  {Lecturer:C} {ReleaseDate.ToShortDateString()}");
+    }
+
+}
